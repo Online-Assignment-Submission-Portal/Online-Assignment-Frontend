c5af1e3adbd368cec1ec3d22bee3270988926312
ShivGitAnsh <shivanshmishra192@gmail.com>
2024-12-22 01:08:00 +0530

    frontend-signup

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>File Submission Portal</h2>
+      <nav>
+        <Link to="/signup">Signup</Link> | <Link to="/signin">Signin</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
