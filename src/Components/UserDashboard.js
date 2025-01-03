b4af798db41923ad4d96d0ff88f52dee5432edc6
Nilay-Chaudhary <nilaychaudhary04@gmail.com>
2025-01-03 15:51:18 +0530

    z index

--- src/Components/UserDashboard.js
+++ src/Components/UserDashboard.js
@@ -223,7 +223,7 @@ const UserDashboard = () => {
           )}
   
           {isModalOpen && (
-            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
               <div className="bg-gray-800 p-6 rounded-lg shadow-lg w-1/3">
                 <h2 className="text-xl font-bold mb-4 text-center">Join a Subject</h2>
                 <input
