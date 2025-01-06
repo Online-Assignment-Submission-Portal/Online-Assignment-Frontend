c294eb06e72479763cf0ebab43b9329ca969ee70
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2025-01-06 15:28:40 +0530

    Changes in Profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -66,7 +66,12 @@ const Profile = () => {
           </button>
         </div>
         <div className="flex justify-between items-center mb-8">
-          <button className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg">
+          <button
+          // onClick={handleDisplayPicture}
+          className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg"
+          accept=".jpeg,.jpg"
+          required
+          >
             Change Photo
           </button>
           <div className="flex justify-between items-center mb-8">
