219267b07caf71691fb2681ce19519ab7e620330
SHASHI SHANKAR <sk7698323@gmail.com>
2025-01-21 09:30:28 +0530

    icons added to the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'remixicon/fonts/remixicon.css'
 
 const Footer = () => {
   return (
@@ -9,16 +10,16 @@ const Footer = () => {
           {/* Column 1: Company Info */}
           <div className="w-full md:w-1/3 mb-4 md:mb-0">
             <h5 className="font-bold text-lg">Company Name</h5>
-            <p className="text-sm">© 2025 All rights reserved.</p>
+            <p className="text-sm"><span className='text-lg text-violet-600'><i class="ri-copyright-line"></i></span> 2025 All rights reserved.</p>
           </div>
 
           {/* Column 2: Quick Links */}
           <div className="w-full md:w-1/3 mb-4 md:mb-0">
             <h5 className="font-bold text-lg">Quick Links</h5>
             <ul className="text-sm">
-            <li className="my-2"><Link to="/about" className="hover:underline">About Us</Link></li>
-            <li className="my-2"><a href="/services" className="hover:underline">Services</a></li>
-              <li className="my-2"><a href="/contact" className="hover:underline">Contact</a></li>
+            <li className="my-2"><Link to="/about" className="hover:underline"><span className='text-lg text-violet-600'><i class="ri-information-line"></i></span> About Us</Link></li>
+            <li className="my-2"><a href="/services" className="hover:underline"><span className='text-lg text-violet-600'><i class="ri-customer-service-fill"></i></span> Services</a></li>
+              <li className="my-2"><a href="/contact" className="hover:underline"><span className='text-lg text-violet-600'><i class="ri-contacts-book-3-fill"></i> </span> Contact</a></li>
             </ul>
           </div>
 
@@ -26,9 +27,9 @@ const Footer = () => {
           <div className="w-full md:w-1/3 mb-4 md:mb-0">
             <h5 className="font-bold text-lg">Follow Us</h5>
             <ul className="text-sm space-y-2">
-              <li><a href="#" className="hover:text-gray-400">Facebook</a></li>
-              <li><a href="#" className="hover:text-gray-400">Twitter</a></li>
-              <li><a href="#" className="hover:text-gray-400">Instagram</a></li>
+              <li><a href="#" className="hover:text-gray-400"><span className='text-lg text-violet-600'><i class="ri-facebook-box-fill"></i></span> Facebook</a></li>
+              <li><a href="#" className="hover:text-gray-400"><span className='text-lg text-violet-600'><i class="ri-twitter-x-line"></i></span> Twitter</a></li>
+              <li><a href="#" className="hover:text-gray-400"><span className='text-lg text-violet-600'> <i class="ri-instagram-line"></i></span> Instagram</a></li>
             </ul>
           </div>
         </div>
