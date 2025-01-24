fff4ae3487880f8e2ce35e05bef452607a676ac9
SHASHI SHANKAR <sk7698323@gmail.com>
2025-01-25 00:01:12 +0530

    contact us page updated part 2

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -20,7 +20,7 @@ const Services = () => {
           </button>
         </div>
       <div className="container mx-auto px-4">
-        <h1 className="text-4xl font-bold text-center text-primary mb-8">Our Services</h1>
+        <h1 className="text-4xl font-bold text-center text-primary mb-8 hover:text-5xl hover:font-blue-600">Our Services</h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
           {/* Assignment Submission */}
