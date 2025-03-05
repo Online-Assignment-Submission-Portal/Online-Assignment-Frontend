b791f6056584bc047c2fb082873870b943a0a416
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2025-03-05 13:22:39 +0530

    Added Console in signin page

--- src/Components/Signin.jsx
+++ src/Components/Signin.jsx
@@ -20,7 +20,7 @@ const Signin = () => {
   const apiUrl =
     window.location.hostname === "localhost"
       ? "http://localhost:8000"
-      : process.env.REACT_APP_BASE_URL;
+      : import.meta.env.REACT_APP_BASE_URL;
   const navigate = useNavigate();
   console.log('hello url', apiUrl);
   const handleSignin = async (e) => {
